e112f1b6139001a5ee9c227d1880dbc0fa44553e
GấuMiLo <117769024+Dtrong101@users.noreply.github.com>
2024-05-09 15:05:12 +0700

    v1
    123

---
+++ lib/widget/app_constant.dart
@@ -0,0 +1,4 @@
+String publishableKey =
+    "pk_test_51PERfeCC2Vbpv9Uaw66eTvBiKiBdO60s4B9HcpkkOU1ZPjWwvXOtSU7cIgL9Wv6rIAZiyIAq5BRWgtUHkJeAqttB00I1Kdcm69";
+String secretKey =
+    "sk_test_51PERfeCC2Vbpv9UaX2BjpFyIV7a9XaGXz0KBXrjr3xYsCsSwckt8l24IxeLibFWHysRfMorD3vMOLBVvqXCVQSia00GF2oC3Fc";
